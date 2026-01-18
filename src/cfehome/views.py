5443277621bccd001151f3b425864eef6f984873
fransteven <fransteven18@hotmail.com>
2026-01-18 11:11:26 -0500

    add change

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -8,7 +8,7 @@ def home(request, *args, **kwargs):
 
     queryset = PageVisits.objects.all()
     my_title = "My Page"
+    html_template = "home.html"
     my_context = {"my_title": my_title, "queryset": queryset, "path": path}
     PageVisits.objects.create()
-    html_template = "home.html"
     return render(request, html_template, my_context)
